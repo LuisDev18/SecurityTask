e6317a69ed7a6e519d23af6e24da6cf3bbfcafb6
LuisDev18 <brian@orbita.pe>
2023-10-15 10:00:57 -0500

    ajustes dto

--- src/main/java/pe/edu/utp/dto/ArticuloDto.java
+++ src/main/java/pe/edu/utp/dto/ArticuloDto.java
@@ -1,6 +1,7 @@
 package pe.edu.utp.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,5 +20,6 @@ public class ArticuloDto {
   @NotBlank(message = "El campo nombre no puede estar vacio")
   private String nombre;
 
+  @NotNull(message = "El campo precio no puede estar vacio")
   private Double precio;
 }
