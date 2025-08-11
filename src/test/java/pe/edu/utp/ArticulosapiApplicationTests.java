d222b4035be22f4353b92135c5afb46dac54f6fb
LuisDev18 <brian@orbita.pe>
2025-08-10 21:43:51 -0500

    fix: correct indentation in ArticulosapiApplicationTests for consistency

--- src/test/java/pe/edu/utp/ArticulosapiApplicationTests.java
+++ src/test/java/pe/edu/utp/ArticulosapiApplicationTests.java
@@ -12,19 +12,19 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 class ArticulosapiApplicationTests {
 
-    @Container
-    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("testdb")
-            .withUsername("testuser")
-            .withPassword("testpass")
-            .withCommand("--log_bin_trust_function_creators=1");
+  @Container
+  static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
+    .withDatabaseName("testdb")
+    .withUsername("testuser")
+    .withPassword("testpass")
+    .withCommand("--log_bin_trust_function_creators=1");
 
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", mySQLContainer::getUsername);
-        registry.add("spring.datasource.password", mySQLContainer::getPassword);
-    }
+  @DynamicPropertySource
+  static void setProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+    registry.add("spring.datasource.username", mySQLContainer::getUsername);
+    registry.add("spring.datasource.password", mySQLContainer::getPassword);
+  }
 
   @Test
   void contextLoads() {}
